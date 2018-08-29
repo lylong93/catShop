63a9092212e8f9579b5d6938a7a393f8b43afaa7
李颖龙 <695498118@qq.com>
2018-08-29 23:30:12 +0800

    .

--- src/components/AddTest/index.js
+++ src/components/AddTest/index.js
@@ -6,16 +6,34 @@ import MailIcon from '@material-ui/icons/Mail';
 import Chip from '@material-ui/core/Chip';
 import DoneIcon from '@material-ui/icons/Done';
 
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 import style from './style.scss'
 
 class AddTest extends Component {
   constructor(props) {
     super(props)
   }
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
    handleDelete() {
     alert('You clicked the delete icon.'); // eslint-disable-line no-alert
   }
   render() {
+    const {value } = this.state
     return (
       <div className={style.wrapper}>
         <div className={style.head}>
@@ -23,13 +41,32 @@ class AddTest extends Component {
            <MailIcon/>
           </Badge>
         </div>
-        <Chip
-        label="Clickable Link Chip"
-        clickable
-        onDelete={this.handleDelete}
-        className={style.chip}
-         deleteIcon={<DoneIcon/>}
-         color="primary"/>
+
+         <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" href="#basic-tabs" />
+          </Tabs>
+        </AppBar>
+
+        {value === 0 && <div>Item One</div>}
+         {value === 1 && <div>Item tow</div>}
+          {value === 2 && <div>Item three</div>}
+         <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>new order</Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                  <Chip
+                  label="Clickable Link Chip"
+                  clickable
+                  onDelete={this.handleDelete}
+                  className={style.chip}
+                   deleteIcon={<DoneIcon/>}
+                   color="primary"/>
+              </ExpansionPanelDetails>
+          </ExpansionPanel>
       </div>
     );
   }
